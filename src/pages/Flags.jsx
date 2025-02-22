85d87a4ed3ba0d4c1c058680c21023dc741cbed8
Rahul Grover <groverrahul47@gmail.com>
2025-02-22 10:13:28 +0530

    final commit

--- src/pages/Flags.jsx
+++ src/pages/Flags.jsx
@@ -122,7 +122,7 @@ const Flags = () => {
                     borderCountries.map((borderCountry) => (
                       <button
                         key={borderCountry.cca3}
-                        className={`${
+                        className={` cursor-pointer${
                           darkMode
                             ? "bg-[#2b3743] text-white"
                             : "bg-white text-black"
