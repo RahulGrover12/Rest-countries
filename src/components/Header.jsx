ade0e35059293702f56462f78a99e170123012fe
Rahul Grover <groverrahul47@gmail.com>
2025-02-23 18:40:18 +0530

    Header edited

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,17 @@ import DarkMode from "./DarkMode";
 import { ThemeContext } from "../context/ThemeContext";
 
 const Header = () => {
-  const { darkMode, setDarkMode } = useContext(ThemeContext);
+  const { darkMode } = useContext(ThemeContext);
+
   return (
     <div
-      className={`w-full h-25 shadow-md flex items-center justify-between 
+      className={`w-full h-25 shadow-md flex items-center justify-between px-5 md:px-10 lg:px-20 
       ${darkMode ? "bg-[#213943] text-white" : "bg-white text-black"}`}
     >
-      <h1 className="font-bold text-xl ml-10 lg:text-4xl">
+      <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl">
         Where in the world?
       </h1>
-      <DarkMode darkMode={darkMode} setDarkMode={setDarkMode} />
+      <DarkMode />
     </div>
   );
 };
