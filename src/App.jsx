5ca82804969f56921383469234deaf55196d83df
Rahul Grover <groverrahul47@gmail.com>
2025-02-23 18:43:29 +0530

    app edited

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Flags from "./pages/Flags";
 import { ThemeProvider } from "./context/ThemeContext";
+import { DataProvider } from "./context/DataContext";
 const App = () => {
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/:countryName" element={<Flags />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <DataProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/:countryName" element={<Flags />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </DataProvider>
   );
 };
 
