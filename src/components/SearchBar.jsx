1985f2b3fb6f871acfcb88a8a48db5aba482b16b
Rahul Grover <groverrahul47@gmail.com>
2025-02-23 18:40:31 +0530

    SearchBar edited

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,32 +2,30 @@ import { CiSearch } from "react-icons/ci";
 import Filter from "./Filter";
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
-import Sorting from "./Sorting";
-const SearchBar = ({
-  searchCountry,
-  setSearchCountry,
-  flagData,
-  setFlagData,
-}) => {
+import { DataContext } from "../context/DataContext";
+
+const SearchBar = () => {
   const { darkMode } = useContext(ThemeContext);
+  const { searchCountry, setSearchCountry } = useContext(DataContext);
+
   return (
-    <div className="flex flex-col gap-5 mt-10 justify-between ml-10 md:flex-row">
+    <div className="flex flex-col gap-5 mt-10 justify-between px-5 md:flex-row md:items-center md:px-10">
       <div
-        className={`flex gap-5 items-center rounded-lg shadow-lg w-[30%] h-[10%]
+        className={`flex gap-5 items-center rounded-lg shadow-lg w-full md:w-[40%] h-[3rem] px-5
         ${darkMode ? "bg-[#213943] text-white" : "bg-white text-black"}`}
       >
-        <CiSearch className="h-7 w-7 ml-5 text-gray-500" />
+        <CiSearch className="h-6 w-6 text-gray-500" />
         <input
           type="text"
-          className={`h-15 w-60 text-xl focus:outline-0 
-            ${darkMode ? "bg-[#213943] text-white" : "bg-white text-black"}`}
+          className={`w-full text-xl focus:outline-0 bg-transparent ${
+            darkMode ? "bg-[#213943] text-white" : "bg-white text-black"
+          }`}
           placeholder="Search for a country"
           value={searchCountry}
           onChange={(e) => setSearchCountry(e.target.value)}
         />
       </div>
-      <Sorting flagData={flagData} setFlagData={setFlagData} />
-      <Filter flagData={flagData} setFlagData={setFlagData} />
+      <Filter />
     </div>
   );
 };
