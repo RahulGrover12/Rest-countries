04b79812d36d832d749ef6b2dea94183cba3baa8
Rahul Grover <groverrahul47@gmail.com>
2025-02-23 18:41:41 +0530

    dark mode responsiveness edited

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,23 +1,21 @@
 import { useContext } from "react";
 import { IoMoonOutline, IoMoonSharp } from "react-icons/io5";
 import { ThemeContext } from "../context/ThemeContext";
+
 const DarkMode = () => {
   const { darkMode, setDarkMode } = useContext(ThemeContext);
+
   return (
-    <div className="flex gap-3 cursor-pointer">
+    <div
+      className="flex items-center gap-2 md:gap-3 cursor-pointer"
+      onClick={() => setDarkMode((prev) => !prev)}
+    >
       {darkMode ? (
-        <IoMoonSharp className="h-6 w-6 mt-[4px] text-gray-500" />
+        <IoMoonSharp className="h-5 w-5 md:h-6 md:w-6 text-gray-400" />
       ) : (
-        <IoMoonOutline className="h-6 w-6 mt-[4px]" />
+        <IoMoonOutline className="h-5 w-5 md:h-6 md:w-6 text-gray-600" />
       )}
-      <p className="text-lg font-medium mr-10 lg:text-xl">
-        <button
-          className="cursor-pointer"
-          onClick={() => setDarkMode((prev) => !prev)}
-        >
-          Dark Mode
-        </button>
-      </p>
+      <p className="text-lg md:text-xl font-medium">Dark Mode</p>
     </div>
   );
 };
